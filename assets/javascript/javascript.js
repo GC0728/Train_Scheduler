d6ce01aa8e47de7ebce4b6473b56199aff3ce39c
unknown <chugm1989@gmail.com>
2019-03-30 15:46:11 -0400

    hw_sub2

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,17 +1,17 @@
 // INITIALIZE FIREBASE
 var config = {
-    apiKey: "AIzaSyCxJUHxWOvmexZk1stnJS6xC7Cd6QvoOk0",
-    authDomain: "train-scheduler-d4cb5.firebaseapp.com",
-    databaseURL: "https://train-scheduler-d4cb5.firebaseio.com",
-    projectId: "train-scheduler-d4cb5",
-    storageBucket: "train-scheduler-d4cb5.appspot.com",
-    messagingSenderId: "685361047223"
-  };
-  firebase.initializeApp(config);
-  console.log(config);
+  apiKey: "AIzaSyCxJUHxWOvmexZk1stnJS6xC7Cd6QvoOk0",
+  authDomain: "train-scheduler-d4cb5.firebaseapp.com",
+  databaseURL: "https://train-scheduler-d4cb5.firebaseio.com",
+  projectId: "train-scheduler-d4cb5",
+  storageBucket: "train-scheduler-d4cb5.appspot.com",
+  messagingSenderId: "685361047223"
+};
+firebase.initializeApp(config);
+console.log(config);
 
 // DECLARE VARIABLE TO CALL FIREBASE DATABASE
-  var database = firebase.database();
+var database = firebase.database();
 
 // GLOBAL VARIABLES
 var train = "";
@@ -21,44 +21,50 @@ var frequency;
 
 // SELECTORS
 
- 
- 
-
- // SUBMIT BUTTON SELECTOR: CLICK EVENT/STORE INPUT VALUE WORK (03/27/19);
+// SUBMIT BUTTON SELECTOR: CLICK EVENT/STORE INPUT VALUE WORK (03/27/19);
 $(".btn").on("click", function(event) {
-    event.preventDefault();
-    var train = $("#inputTrain").val().trim();
-    var destination = $("#inputDestination").val().trim();
-    var frequency = $("#inputFrequency").val().trim();
-    var time = $("#inputTime").val().trim();
+  event.preventDefault();
+  var train = $("#inputTrain")
+    .val()
+    .trim();
+  var destination = $("#inputDestination")
+    .val()
+    .trim();
+  var frequency = $("#inputFrequency")
+    .val()
+    .trim();
+  var time = $("#inputTime")
+    .val()
+    .trim();
 
-// STORE INPUT VALUES TO FIREBASE
-    database.ref().set({
-        train: train,
-        destination: destination,
-        frequency: frequency,
-        time: time
-});
+  // STORE INPUT VALUES TO FIREBASE
+  database.ref().push({
+    train: train,
+    destination: destination,
+    frequency: frequency,
+    time: time
+  });
 });
 
 // WRITE FROM FIREBASE TO HTML
-    database.ref().on("value", function(snapshot) {
-        event.preventDefault();
-        var nexTrain = minutesPending(snapshot.val().time, snapshot.val().frequency);
-      //  $("#schedule").empty();
-        $("#schedule").append(`
+database.ref().on("child_added", function(childSnapshot) {
+  event.preventDefault();
+  var nexTrain = minutesPending(childSnapshot.val().time, childSnapshot.val().frequency);
+  console.log(childSnapshot);
+  $("#schedule").append(`
     <tr> 
-        <th> ${snapshot.val().train} </th> 
-        <td> ${snapshot.val().destination} </td> 
-        <td> ${snapshot.val().time} </td> 
-        <td> ${snapshot.val().frequency} </td> 
+        <th> ${childSnapshot.val().train} </th> 
+        <td> ${childSnapshot.val().destination} </td> 
+        <td> ${childSnapshot.val().time} </td> 
+        <td> ${childSnapshot.val().frequency} </td> 
         <td> ${nexTrain} </td>
     </tr> 
         `);
-        $(".form-control").val("");
-    });
+  $(".form-control").val("");
+  console.log(nextTrain)
+});
 
-// STORE INPUT VALUES IN ARRAY    
+// STORE INPUT VALUES IN ARRAY
 /*  scheduler.push(train);
     scheduler.push(destination);
     scheduler.push(time);
@@ -68,7 +74,7 @@ $(".btn").on("click", function(event) {
 
 // APPEND USER INPUT TO TRAIN SCHEDULER
 // for (i = 0; i < scheduler.length; i++) {
- /*   var newTrain = $("<th>");
+/*   var newTrain = $("<th>");
     newTrain.text(scheduler[0]);
  $("#schedule").append(`
 <tr> 
@@ -83,31 +89,26 @@ $(".btn").on("click", function(event) {
 $(".form-control").val("");
 // scheduler.length = 0;
 
-
 // MOMENT.JS
 function minutesPending(firstTrain, frequency) {
-    var realTime = moment();
-    var openShift = moment(firstTrain, "HH:mm");
+  var realTime = moment();
+  var openShift = moment(firstTrain, "HH:mm");
+  var timeRemainder = 0;
+  console.log(realTime);
 
-    var timeRemainder = 0;
-console.log(realTime);
-
-if (openShift > realTime) {
+  if (openShift > realTime) {
     timeRemainder = openShift.diff(realTime, "minutes");
-} else {
+  } else {
     var minutesPast = realTime.diff(openShift, "minutes");
     var remainder = minutesPast % frequency;
     timeRemainder = frequency - remainder;
-    console.log(minutesTilNextTrain);
-    nextTrain = realTime.add(minutesTilNextTrain, "minutes");
+    console.log(timeRemainder);
+    nextTrain = realTime.add(timeRemainder, "minutes");
     console.log(nextTrain);
+  }
+  console.log("Next Train Arrival Time:", nextTrain.format("mm"));
+  return nextTrain;
 }
-console.log("Next Train Arrival Time:", nextTrain.format("hh:mm A"));
-return minutesPending;
+// console.log(minutesPending);
 
-};
-console.log(minutesPending);
-
-
-
-moment().format("hh:mm:ss a");
+ moment().format("hh:mm:ss a");
