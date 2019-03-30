8718461cabc628cfcfd5ca781ebbf00cd785c8fc
unknown <chugm1989@gmail.com>
2019-03-29 21:27:15 -0400

    firebase-working

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -9,11 +9,14 @@ var config = {
   };
   firebase.initializeApp(config);
   console.log(config);
+
+// DECLARE VARIABLE TO CALL FIREBASE DATABASE
   var database = firebase.database();
+  console.log(database);
 
 // GLOBAL VARIABLES
-var train;
-var destination;
+var train = "";
+var destination = "";
 var trainTime;
 var frequency;
 var scheduler = [];
@@ -24,31 +27,48 @@ var scheduler = [];
  
 
  // SUBMIT BUTTON SELECTOR: CLICK EVENT/STORE INPUT VALUE WORK (03/27/19);
-$(".btn").on("click", function() {
+$(".btn").on("click", function(event) {
     event.preventDefault();
     var train = $("#inputTrain").val().trim();
     var destination = $("#inputDestination").val().trim();
     var frequency = $("#inputFrequency").val().trim();
-    var nextTrain = $("#inputNext").val().trim();
     var time = $("#inputTime").val().trim();
+
+// STORE INPUT VALUES TO FIREBASE
+    database.ref().set({
+        train: train,
+        destination: destination,
+        frequency: frequency,
+        time: time
+})
 
 // STORE INPUT VALUES IN ARRAY    
     scheduler.push(train);
     scheduler.push(destination);
-    scheduler.push(frequency);
-    scheduler.push(nextTrain);
     scheduler.push(time);
+    scheduler.push(frequency);
     console.log(scheduler);
 
-// APPEND USER INPUT TO SCHEDULER
+
+// APPEND USER INPUT TO TRAIN SCHEDULER
 var newTrain = $("<th>");
 newTrain.text(scheduler[0]);
 // for (i = 0; i < scheduler.length; i++) {
 $("#schedule").append(`
-<tr> <th> ${scheduler[0]} </th> <td> ${scheduler[1]} </td> <td> ${scheduler[2]} </td> <td> ${scheduler[3]} </td> <td> ${scheduler[4]} </td>`);
-   
-$(".form-control").val("");  
+<tr> 
+    <th> ${scheduler[0]} </th> 
+    <td> ${scheduler[1]} </td> 
+    <td> ${scheduler[2]} </td> 
+    <td> ${scheduler[3]} </td> 
+</tr> 
+`);
+
+// RESET FORM/CLEAR ARRAY
+$(".form-control").val("");
+scheduler.length = 0;
+
 });
 
 
-// RESET FORM FUNCTION
+
+
